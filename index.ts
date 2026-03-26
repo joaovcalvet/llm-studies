509117b5d2a4450e5122b9a49698a554663c74c5
joaovcalvet <joaovictor.calvet@outlook.com>
2026-03-26 18:07:47 -0300

    [Feat]: get categories endpoint and category_id search in brands

--- index.ts
+++ index.ts
@@ -19,17 +19,19 @@ async function main()
             const question = await rl.question('Your prompt: ');
             contents.push({ role: 'user', content: question })
 
+            let countCalls = 0;
             while(true)
             {
+                console.log("Chamadas: " + ++countCalls);
                 const result = await ai.responses.create({
                     model: process.env.GROQ_MODEL!,
-                    input: contents,
+                    input: JSON.stringify(contents),
                     tools: tools
                 });
     
                 contents.push(result.output);
     
-                if(result.output_text !== '') console.log("\n\n" + result.output_text);
+                if(result.output_text) console.log("\n\n" + result.output_text);
 
                 let call: boolean = false;
                 for(const item of result.output)
@@ -41,13 +43,21 @@ async function main()
                     const handler = toolMap[item.name];
                     if(handler)
                     {
-                        const result = await handler();
+                        const result = await handler(JSON.parse(item.arguments));
       
                         contents.push({
                             type: "function_call_output",
                             call_id: item.call_id,
                             output: JSON.stringify(result)
                         });
+                    }
+                    else
+                    {
+                        contents.push({
+                            type: "function_call_output",
+                            call_id: item.call_id,
+                            output: "função não encontrada"
+                        })
                     }
                 }
 
@@ -77,36 +87,115 @@ interface Brand {
     descrmarcarmd: string,
 }
 
+interface Category {
+    slug: string,
+    imagem: string,
+    descrgrupoprod: string,
+    codgrupopai: number,
+    codgrupoprod: number,
+    subCategories: SubCategory[]
+}
+
+interface SubCategory {
+    slug: string,
+    imagem: string,
+    descrgrupoprod: string,
+    codgrupopai: number,
+    codgrupoprod: number,
+    countProducts: number,
+    analitico: boolean
+}
+
 //Definição das tools
 const setGetBrands = {
     type: "function" as const,
     name: "get_brands",
     description: "Endpoint utilizado para retornar todas as marcas ativas da Lindacor.",
+    parameters: {
+        type: "object",
+        properties: {
+            group: {
+                type: ["number", "null"],
+                description: "Identificador único (ID) de uma categoria ou sub-categoria."
+            }
+        },
+        required: ["group"],
+        additionalProperties: false
+    },
+    strict: true
+}
+
+const setGetCategories = {
+    type: "function" as const,
+    name: "get_categories",
+    description: "Endpoint utilizado para retornar todas as categorias ativas da Lindacor.",
+    parameters: {
+        type: "object",
+        properties: {
+            group: {
+                type: ["number", "null"],
+                description: "Identificador único (ID) de uma categoria ou sub-categoria."
+            }
+        },
+        required: ["group"],
+        additionalProperties: false
+    },
+    strict: true
+}
+
+const setGetFactories = {
+    type: "function" as const,
+    name: "get_categories",
+    description: "Endpoint utilizado para retornar todas as fábricas ativas da Lindacor.",
     parameters: {},
     strict: true
 }
 
 const tools = [
-    setGetBrands
+    setGetBrands,
+    setGetCategories
 ];
 
 const toolMap: Record<string, Function> = {
-    get_brands: () => getBrands()
+    get_brands: ({ group }: { group?: number }) => getBrands(group),
+    get_categories: ({ group }: { group?: number }) => getCategories(group)
 }
 
 //Definição das funções
-async function getBrands()
+async function getBrands(group: number | undefined)
 {
-    const url = "https://8nyb7laid8.execute-api.us-east-1.amazonaws.com/v1/brands";
+    let search: string = "";
+    if(group != null && group !== 0) search = `/${group}`;
+
+    const url = `${process.env.API_URL || ''}/brands${search}`;
     const init: RequestInit = {
         method: "GET",
         headers: {
             'Content-Type': 'application/json',
-            'x-api-key': '6Be4QUaU023RqEqsh7ftV7lINnCKr4fk267ct6IH',
+            'x-api-key': process.env.API_KEY || '',
             'x-tenant-id': '2'
         }
     }
 
     const result: APIResponse & { data: Brand[] } = await (await fetch(url, init)).json();
+    return result;
+}
+
+async function getCategories(group: number | undefined)
+{
+    let search: string = "";
+    if(group != null && group !== 0) search = `/${group}`;
+
+    const url = `${process.env.API_URL || ''}/categories${search}`;
+    const init: RequestInit = {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': process.env.API_KEY || '',
+            'x-tenant-id': '2'
+        }
+    }
+
+    const result: APIResponse & { data: Category[] | SubCategory } = await (await fetch(url, init)).json();
     return result;
 }
